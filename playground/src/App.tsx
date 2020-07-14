93e17c5befa77085491905375a589671a714f8f8
Roland Branten <roland.branten@gmail.com>
2020-07-14 20:11:33 +0200

    changed examples to playground env

---
+++ playground/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import "./app.css";
+
+const Wrapper = styled.div`
+  padding: 5em 5em 0;
+`;
+
+const App = () => {
+  return <Wrapper>Hello world</Wrapper>;
+};
+
+export default App;
